3529ee6135318f475f66b287f28b66901382a1f5
Kiralearning <0.nomi.ok@gmail.com>
2025-02-23 20:00:07 +0100

    metoda z argumantami tablicy

---
+++ oop2dt/p2p9.java
@@ -0,0 +1,30 @@
+package oop2dt;
+import java.util.Scanner;
+public class p2p9 {
+    public static void main(String[] arg)
+	{
+		int[] tab=new int[7];
+		czytanie(tab);
+		int wynik=oblicz(tab);
+		System.out.println("Wynik to "+wynik);
+	}
+	public static void czytanie(int[] a)
+	{
+		for (int i=0; i<a.length;i++)
+		{System.out.print("podaj elemnt tablicy nr "+i+": ");
+		a[i]=getInt();
+		}
+	}
+	
+	public static int oblicz(int[] a)
+	{
+		int licz=0;
+		for(int i=0; i<a.length; i++)
+		{
+			if (a[i]>0) {licz++;}
+		}
+		return licz;
+	}
+	public static int getInt()
+	{return new Scanner(System.in).nextInt();}
+}
