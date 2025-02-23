495ae16ec1899e2d56f87c38ce4b6f5459e99455
Kiralearning <0.nomi.ok@gmail.com>
2025-02-23 15:47:29 +0100

    p2p1.java

---
+++ oop2dt/p2p1.java
@@ -0,0 +1,15 @@
+package oop2dt;
+
+public class p2p1 {
+   	public static void main(String[] arg)
+	{
+		final int STALA=23;
+		System.out.println("Witaj świecie");
+		System.out.print("Witamy");
+		int i=10;
+		
+		System.out.print("Wynik to "+i*STALA);
+	}
+
+}
+
